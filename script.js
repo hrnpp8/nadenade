1604a5b91a6ab0367ff493cc5a723de92f07fd55
hrnpp8 <78437590+hrnpp8@users.noreply.github.com>
2021-08-08 09:43:01 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,55 @@
+window.onload = function() {
+    //フルスクリーンにする
+        if (this.webkitRequestFullScreen) {
+             this.webkitRequestFullScreen();
+        }
+        else if (this. mozRequestFullScreen) {
+            this. mozRequestFullScreen();
+        }
+};
+
+let p1 = document.getElementById('p1');
+let te = document.getElementById('te');
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+
+let i=0;
+let c=0;
+let nc;
+
+let nnc;
+
+//なでる
+btn1.addEventListener('click',function(){
+
+btn1.style.backgroundColor = "#ccc";
+
+for(nc=0; nc<3; nc++){
+ i +=100;
+ setTimeout('te.style.left = "150px"', i);
+ i +=100;
+ setTimeout('te.style.left = "180px"', i);
+ i +=100;
+ setTimeout('te.style.left = "210px"', i);
+ i +=100;
+ setTimeout('te.style.left = "180px"', i);
+}
+i +=100;
+setTimeout('btn1.style.backgroundColor = "#fff"', i);
+c++;
+console.log(c);
+
+
+if(2<c){
+ i +=200;
+ setTimeout('p1.src="img/2.png"', i);
+}
+});
+
+//なでない
+btn2.addEventListener('click',function(){
+　p1.src="img/3.png";
+});
+
+
+
